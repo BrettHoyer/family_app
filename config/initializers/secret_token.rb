368a3633756b7ea2588a83072195c16855783978
Brett Hoyer <jbh5079@gmail.com>
2012-08-09 13:10:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyApp::Application.config.secret_token = '3c1e889d6f822977eed2eebb327e0cd4f68151eb2d7f13fe0f7531660f8db5ea1d0b495371ad8944b7bcb23f9645e4b2676608f2c7851cde0655956899f27a65'
