192bf02b0cf1db48b71c839b6efd2dd32ddfe620
Brett Hoyer <jbh5079@gmail.com>
2012-08-09 19:51:45 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let(:base_title) { "Brett Hoyer App" }
+
   describe "Home Page" do
 
   	it "should have the h1 'Home'" do
@@ -12,8 +14,13 @@ describe "Static Pages" do
   	it "should have the title 'Home'" do
   		visit '/static_pages/home'
   		page.should have_selector('title',
-  						 :text => "Brett Hoyer App | Home")
+  						 :text => "Brett Hoyer App")
   	end
+
+  	it "should not have a custom page title" do
+  		visit '/static_pages/home'
+  		page.should_not have_selector('title', :text => '| Home')
+  	end	
   end
 
   describe "Help Page" do
@@ -26,7 +33,7 @@ describe "Static Pages" do
   	it "should have the title 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('title',
-  					 	:text => "Brett Hoyer App | Help" )
+  					 	:text => "#{base_title} | Help" )
   	end
   end
 
@@ -41,7 +48,7 @@ describe "Static Pages" do
   	it "should have the title 'Parents'" do
   		visit '/static_pages/parents'
   		page.should have_selector('title',
-  							:text => "Brett Hoyer App | Parents")
+  							:text => "#{base_title} | Parents")
 	end  
   end
 end
